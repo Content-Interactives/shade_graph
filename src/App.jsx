3ef89704281866f1403ca912adbc7b82fd067ecd
User <user@example.com>
2026-02-25 14:16:59 -0800

    added app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ShadeGraph from './components/ShadeGraph'
+
+
+function App() {
+  return (
+      <div className="App">
+        <ShadeGraph />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/ShadeGraph.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+const ShadeGraph = () => {
+
+
+	return (
+        <div>test</div>
+)
+};
+
+
+export default ShadeGraph;
